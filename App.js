954e7942d5cc8489d5931e38bdfd471e35fa526f
inbum <1234!@>
2022-05-03 12:33:28 +0900

    2.9 section 완료 - 2장 끝

--- App.js
+++ App.js
@@ -9,13 +9,18 @@
 import React, {useState} from 'react';
 import {SafeAreaView, Button, StyleSheet} from 'react-native';
 import Box from './components/Box';
+import Counter from './components/Counter';
 
 const App = () => {
-    const [visible, setVisible] = useState(true);
-    const onPress = () => {
-        setVisible(!visible);
-    }
+    // const [visible, setVisible] = useState(true);
+    const [count, setCount] = useState(0);
 
+    const onIncrease = () => setCount(count + 1);
+    const onDecrease = () => setCount(count - 1);
+
+    // const onPress = () => {
+    //     setVisible(!visible);
+    // }
   return (
       // <SafeAreaView>
       //     {/* 둘 중에 하나 사용~ */}
@@ -25,7 +30,7 @@ const App = () => {
       // </SafeAreaView>
 
       <SafeAreaView style={styles.full}>
-
+          <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease}/>
       </SafeAreaView>
   )
 }
